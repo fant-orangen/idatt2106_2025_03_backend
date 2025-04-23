3b929f9b6ac6d072de46e290fae77ebed15faf52
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-04-23 09:40:11 +0200

    chore: Set up backend project with basic dependencies.

---
+++ backend/src/test/java/stud/ntnu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.ntnu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
