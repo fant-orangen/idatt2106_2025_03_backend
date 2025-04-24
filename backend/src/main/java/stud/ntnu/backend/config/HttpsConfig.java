b28b0f3fe995cbd5144d772c6758a6ade76cac16
Sara <saratag@ntnu.no>
2025-04-24 11:11:10 +0200

    SCRUM-320 feat: create configuration file for redirection of http to https

---
+++ backend/src/main/java/stud/ntnu/backend/config/HttpsConfig.java
@@ -0,0 +1,29 @@
+/**
+ * Configuration file to ensure that users entering "http://" are automatically redirected to https://
+ */
+
+package com.krisefikser.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedirectConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return server -> server.addAdditionalTomcatConnectors(httpConnector());
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+}
