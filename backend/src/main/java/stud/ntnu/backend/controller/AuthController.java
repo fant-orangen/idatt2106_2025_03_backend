0ccf5137a0a18b70746fb4296b5bd60ef299f0ae
Brahim Helland <brahimh@stud.ntnu.no>
2025-05-01 16:06:44 +0200

    refactor: removed unused import [SCRUM-444]

--- backend/src/main/java/stud/ntnu/backend/controller/AuthController.java
+++ backend/src/main/java/stud/ntnu/backend/controller/AuthController.java
@@ -2,7 +2,6 @@ package stud.ntnu.backend.controller;
 
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
