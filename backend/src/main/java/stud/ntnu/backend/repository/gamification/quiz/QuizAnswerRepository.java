647e9991a1935adb6c3192ec07550e9d8826b33a
Brahim Helland <brahimh@stud.ntnu.no>
2025-05-06 09:51:29 +0200

    feat(games): added new repository for QuizAnswer [SCRUM-201]

--- backend/src/main/java/stud/ntnu/backend/repository/gamification/quiz/QuizAnswerRepository.java
+++ backend/src/main/java/stud/ntnu/backend/repository/gamification/quiz/QuizAnswerRepository.java
@@ -1,7 +1,10 @@
 package stud.ntnu.backend.repository.gamification.quiz;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import stud.ntnu.backend.dto.quiz.QuizAnswerDto;
 import stud.ntnu.backend.model.gamification.quiz.QuizAnswer;
 
 import java.util.List;
@@ -9,4 +12,8 @@ import java.util.List;
 @Repository
 public interface QuizAnswerRepository extends JpaRepository<QuizAnswer, Long> {
     List<QuizAnswer> findAllByQuestionId(Long questionId);
+    @Query("SELECT new stud.ntnu.backend.dto.quiz.QuizAnswerDto(q.id, q.quizId, q.questionId, q.answerBody, q.isCorrect, q.createdAt) " +
+        "FROM QuizAnswer q WHERE q.questionId = :questionId")
+    List<QuizAnswerDto> findAllAnswersByQuestionId(@Param("questionId") Long questionId);
+
 } 
