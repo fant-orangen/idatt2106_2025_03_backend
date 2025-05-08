03ce87d01a9904985f6073c23e2ddf156a9eec65
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-05-08 16:53:55 +0200

    docs: Add docs to model/news and user

--- backend/src/main/java/stud/ntnu/backend/model/user/Role.java
+++ backend/src/main/java/stud/ntnu/backend/model/user/Role.java
@@ -1,54 +1,102 @@
 package stud.ntnu.backend.model.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+
 import java.util.List;
 
+/**
+ * Entity class representing a user role in the system.
+ * Roles are used to define different access levels and permissions for users.
+ */
 @Entity
 @Table(name = "roles")
 public class Role {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-  @Column(name = "name", nullable = false, unique = true, length = 50)
-  private String name;
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
 
-  @OneToMany(mappedBy = "role")
-  @JsonIgnore
-  private List<User> users;
+    @OneToMany(mappedBy = "role")
+    @JsonIgnore
+    private List<User> users;
 
-  // Constructors
-  public Role() {
-  }
+    /**
+     * Default constructor required by JPA.
+     */
+    public Role() {
+    }
 
-  public Role(String name) {
-    this.name = name;
-  }
+    /**
+     * Constructs a new Role with the specified name.
+     *
+     * @param name the name of the role
+     */
+    public Role(String name) {
+        this.name = name;
+    }
 
-  // Getters and Setters
-  public Integer getId() {
-    return id;
-  }
+    /**
+     * Gets the unique identifier of the role.
+     *
+     * @return the role ID
+     */
+    public Integer getId() {
+        return id;
+    }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+    /**
+     * Sets the unique identifier of the role.
+     *
+     * @param id the role ID to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    /**
+     * Gets the name of the role.
+     *
+     * @return the role name
+     */
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    /**
+     * Sets the name of the role.
+     *
+     * @param name the role name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public List<User> getUsers() {
-    return users;
-  }
+    /**
+     * Gets the list of users associated with this role.
+     *
+     * @return the list of users
+     */
+    public List<User> getUsers() {
+        return users;
+    }
 
-  public void setUsers(List<User> users) {
-    this.users = users;
-  }
+    /**
+     * Sets the list of users associated with this role.
+     *
+     * @param users the list of users to set
+     */
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
